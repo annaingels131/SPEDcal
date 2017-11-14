7810e7a0b36d85c11e6ced705a25532f2a958d72
Anna Ingels <annaingels@Annas-MacBook-Pro.local>
2017-11-13 18:33:42 -0800

    added logo and started dashboard

---
+++ src/pages/dashboard/dashboard.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+
+@IonicPage()
+@Component({
+  selector: 'dashboard',
+  templateUrl: 'dashboard.html'
+})
+export class Dashboard {
+
+
+}
